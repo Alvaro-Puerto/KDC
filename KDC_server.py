79a8575c93ab131a60040fff2e53b00ca92f21b2
Ale-Puerto <alvaroevenor.puertotellez@gmail.com>
2020-07-25 13:05:19 -0600

    multiconexion cliente-KDC

--- KDC_server.py
+++ KDC_server.py
@@ -1,24 +1,47 @@
 import socket
+import json
+import os
 
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad, pad
+
+from _thread import *
+
+
+def decifrando_peticion(data):
+    
+
+
+def hilos_clientes(cliente,addrs):
+    cliente.send('Conectando con KDC '.encode())
+    while True:
+        try:
+            data = cliente.recv(1024)
+            if not data :
+                break
+
+            print('Se ha iniciado una conexion con  ',addrs )
+            cliente.sendall('response'.encode())
+            decifrando_peticion(data)
+
+            break
+        except ConnectionResetError:
+            print('Conexion reiniciada ')
 
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as conexion:
-        conexion.bind(('192.168.1.1', 5000))
+        conexion.bind(('', 5000))
         conexion.listen()
 
-        conn, addr = conexion.accept()
-
-        with conn:
-            print('Conectado a : ', addr)
-            while True:
-                data = conn.recv(1024)
-
-                if not data:
-                    break
-
-                print(data)
-
+        while True:
+            cliente , addrs = conexion.accept()
+            
+            start_new_thread(hilos_clientes,(cliente, addrs))
+        
+        conexion.close()
 
 if __name__ == "__main__":
     main()
+    
--- cliente.py
+++ cliente.py
@@ -1,18 +1,59 @@
 import socket
 import os
+import random
+import json
 
+from base64 import b64encode
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+
+
+KEY = b'?@\xf2\x86\xccB\xdb\xf3\xba\xaf\x0f%w\x9fk\x9c'
 
 def borrar_pantalla():
     os.system("clear")
 
-def conectando_kdc(*args):
-    ip_destino = args
+def conectando_kdc(json_request):
+    json_request = json_request
 
     #CREANDO SOCKET
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as kdc_conexion:
-        kdc_conexion.connect(('192.168.1.1', 5000))
-        kdc_conexion.sendall(b'Hola es mi primer mensaje')
-        data = kdc_conexion.recv(1024)
+        kdc_conexion.connect(('', 5000))
+        while True:
+            kdc_conexion.sendall(json_request.encode())
+            data = kdc_conexion.recv(1024)
+            if not data :
+                break
+            try:
+                print(json.loads(data.decode()))
+                break
+            except Exception as e:
+                print(data.decode())
+
+            break
+        kdc_conexion.close()
+        input()
+
+
+def cifrando_mensaje(ip_destino):
+    ip_destino = ip_destino
+    nonce = random.randint(1,10000)
+
+    body = json.dumps({
+        'ip' : ip_destino,
+        'nonce': nonce
+    })
+    
+    bloque = AES.new(KEY, AES.MODE_CBC)
+    
+    cripto = bloque.encrypt(pad(body.encode(), AES.block_size))
+    iv = b64encode(bloque.iv).decode()
+    body = b64encode(cripto).decode()
+
+    json_request = json.dumps({'iv':iv, 'body':body})
+    conectando_kdc(json_request)
 
 
 def main():
@@ -29,11 +70,13 @@ def main():
             if op == 2:
                 try:
                     ip_destino = input('Ingrese el ip del servidor a entablar conexion ')
-                    conectando_kdc(ip_destino) # Funcion que crea un socket entre KDC y el cliente 
+                    cifrando_mensaje(ip_destino) # Funcion que crea un socket entre KDC y el cliente 
 
                 except Exception as e:
+                    input()
                     borrar_pantalla()
                     print('Error datos invalidos :', ip_destino)
+                    print(e)
                     input()
 
             elif op == 1:
